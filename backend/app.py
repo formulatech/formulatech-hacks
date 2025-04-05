e9a2b2cb18f815952faa2e9bc10fa00463ff805d
Hamza Ali <github+hhhapz@hamza.sh>
2025-04-05 15:16:36 -0400

    feat: initial commit with backend

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, Response, jsonify
+
+app = Flask(__name__)
+name = "FormulaTech-Hacks"
+mode: str = "Debug" if app.debug else "Production"
+
+
+@app.route("/")
+def root() -> Response:
+	return jsonify({"mode": mode, "name": name})
