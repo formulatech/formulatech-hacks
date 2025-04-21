bd632d90af495d4b7cdee0b3287f06fc6e24f05b
Hamza Ali <github+hhhapz@hamza.sh>
2025-04-21 18:46:59 -0400

    feat: add hero page

---
+++ apps/www/src/components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Component() {
+    return (
+        <main className="grid place-items-center bg-red-200 h-screen">
+            <h1 className="text-3xl">
+                FormulaTech Hacks wooo
+            </h1>
+        </main>
+    )
+}
