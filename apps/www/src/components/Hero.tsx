91a0b435902c56ed4caedb3f8ebb067707883abd
Hamza Ali <github+hhhapz@hamza.sh>
2025-04-21 18:51:32 -0400

    feat: add hero page (#17)
    * feat: add hero page
    
    * chore: fix color

---
+++ apps/www/src/components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Component() {
+    return (
+        <main className="grid place-items-center bg-blue-300 h-screen">
+            <h1 className="text-3xl">
+                FormulaTech Hacks wooo
+            </h1>
+        </main>
+    )
+}
