6162e8b6e0c79af49ea5e8df2397b7435e4495aa
Linda Chen <143955921+Lindoop@users.noreply.github.com>
2025-04-22 10:52:32 -0400

    feat: basic coming soon page (#5)
    * feat: basic coming soon page
    
    * feat: updated so that the styles sans the keyframe animations are now listed in the class
    
    ---------
    
    Co-authored-by: Linda Chen <lindachen@Lindas-MacBook-Pro.local>

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [ "./apps/www/src/**/*.{html,js,astro,jsx,ts,tsx}" ],
+    theme: {
+        extend: {
+            colors: {
+                background: '#FFFCF3',
+                focus: '#D2273D',
+
+                accent: {
+                    1: '#00AEB9',
+                    2: '#383838',
+                    3: '#000017'
+                },
+            },
+            
+            fontFamily: {
+                primary: ['"Primary Font"', 'sans-serif'],
+                secondary: ['"Secondary Font"', 'sans-serif'],
+            },
+
+            fontWeight: {
+                regular: '400',
+                medium: '500',
+                bold: '700',
+            },
+        },
+    },        
+    plugins: [],
+}
