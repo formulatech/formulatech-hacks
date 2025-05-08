3735ed410313974dbb27498a45b71766a1eedf70
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2025-05-08 15:49:03 -0400

    feat: start of hero page

--- apps/www/src/components/Hero.tsx
+++ apps/www/src/components/Hero.tsx
@@ -1,7 +1,18 @@
-export default function Component() {
-  return (
-    <main className="grid place-items-center bg-road h-screen">
-      <h1 className="font-black p-10 text-6xl capitalize text-white bg-clip-text bg-gradient-to-r from-primary to-secondary stroke-xl font-title">FORMULATECH HACKS</h1>
-    </main>
-  )
+
+import "../styles/Hero.css";
+
+
+export default function Hero() {
+    return (
+        <div className="background-container">
+            <div className="main-wrapper">
+                <h1 className="title-text">
+                    FormulaTech Hacks
+                </h1>
+                <button className="apply-button"> Register now!</button>
+            </div>
+        </div>
+    );
 }
+
+
