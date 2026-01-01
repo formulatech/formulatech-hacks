b5370fac906b92f14eccbcf28700fafee5b9c723
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2025-12-31 20:16:51 -0500

    Change teams section title

--- apps/www/src/components/Teams.tsx
+++ apps/www/src/components/Teams.tsx
@@ -63,8 +63,14 @@ export default function Teams() {
         <>
             <div className="mt-[5dvh] pt-5 w-[80%] mx-auto flex flex-col gap-[30px] bg-[#fafbee]" id="teams">
                 <img src={blueStar.src} alt="Blue Star" className="absolute top-0 left-0 w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 xl:w-56 xl:h-56 -mt-10 -ml-10 z-0" />
-                <h1 className="font-black p-5 -ml-2.5 text-5xl capitalize text-foreground stroke-xl font-title">
-                    THE TEAM
+                <h1
+                    className="flex flex-col md:flex-row items-center font-black px-3 py-4 md:p-5 sm:ml-0 lg:ml-15 font-title capitalize text-center leading-tight"
+                    style={{ fontSize: "clamp(1.4rem, 4vw, 2.75rem)" }}>
+                    <span className="mr-6 bg-clip-text text-transparent bg-gradient-to-t from-primary to-secondary hidden md:block">/</span>
+                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
+                        MEET THE TEAM
+                    </span>
+                    <span className="ml-6 bg-clip-text text-transparent bg-gradient-to-t from-primary to-secondary hidden md:block">/</span>
                 </h1>
             </div>
 
