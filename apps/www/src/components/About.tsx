fe9396e2f9e1ee0f7701dbbe5b6730b616a67d08
Marwa Zaryab <118633267+marwazaryab@users.noreply.github.com>
2025-12-31 19:44:50 -0500

    Update about section text

--- apps/www/src/components/About.tsx
+++ apps/www/src/components/About.tsx
@@ -3,16 +3,22 @@ import car from "../assets/car_top.svg";
 export default function About() {
     return (
         <div
-            className="mt-[10dvh] py-8 flex flex-col items-center justify-center min-h-screen"
+            className="mt-[10dvh] py-8 flex flex-col items-center justify-center min-h-screen bg-[#fafbee]"
             id="about"
         >
             <div className="w-[80%] mx-auto flex flex-col lg:flex-row items-center lg:items-start justify-between gap-8">
                 {/* Text Section */}
                 <div className="flex flex-col items-center lg:items-start gap-[30px]">
-                    <h1 className="font-black p-5 -ml-2.5 text-5xl capitalize text-white bg-clip-text bg-gradient-to-r from-primary to-secondary stroke-xl font-title">
+                <h1
+                className="flex flex-col md:flex-row items-center font-black px-3 py-4 md:p-5 sm:ml-0 lg:ml-15 font-title capitalize text-center leading-tight"
+                style={{ fontSize: "clamp(1.4rem, 4vw, 2.75rem)" }}>
+                    <span className="mr-6 bg-clip-text text-transparent bg-gradient-to-t from-primary to-secondary hidden md:block">/</span>
+                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
                         ABOUT
-                    </h1>
-                    <p className="font-mono text-xl sm:text-2xl md:text-xl text-white leading-relaxed text-center lg:text-left">
+                    </span>
+                    <span className="ml-6 bg-clip-text text-transparent bg-gradient-to-t from-primary to-secondary hidden md:block">/</span>
+                </h1>
+                    <p className="font-arial text-xl sm:text-2xl md:text-xl text-black leading-relaxed text-center lg:text-left">
                         FormulaTech Hacks creates an energizing space where
                         motorsport enthusiasts can thrive, fostering an
                         environment that accelerates learning and ignites
