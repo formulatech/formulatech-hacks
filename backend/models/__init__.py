c83aec08837d8ee24b7e87e6bf24d77c9eb12c21
Richard Li <79170351+RichardLi-1@users.noreply.github.com>
2026-01-11 10:50:00 -0500

    feat(backend): export domain models from package

---
+++ backend/models/__init__.py
@@ -0,0 +1,6 @@
+from backend.models.registration import Registration
+from backend.models.sponsor import Sponsor
+from backend.models.team import Team
+from backend.models.user import User
+
+__all__ = ["Registration", "Sponsor", "Team", "User"]
